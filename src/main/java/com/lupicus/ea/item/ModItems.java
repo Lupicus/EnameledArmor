0dd6d2e5aed71b8edc0342e7b5e98af9ea0ceead
Lupicus <54924432+Lupicus@users.noreply.github.com>
2021-12-04 10:08:23 -0600

    Correct netherite sword

--- src/main/java/com/lupicus/ea/item/ModItems.java
+++ src/main/java/com/lupicus/ea/item/ModItems.java
@@ -24,7 +24,8 @@ public class ModItems
 	// 	Swords have no color- just enchantment glint.
     public static final Item EA_DIAMOND_SWORD = new EASwordItem(ItemTier.DIAMOND, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT),"ea_diamond_sword");
     public static final Item EA_BOW = new EABowItem((new Item.Properties()).maxDamage(384).group(ItemGroup.COMBAT),"ea_bow");
-	private static final IArmorMaterial CH_MATERIAL = new EAArmorMaterial(ArmorMaterial.CHAIN,"ea_chainmail");
+
+    private static final IArmorMaterial CH_MATERIAL = new EAArmorMaterial(ArmorMaterial.CHAIN,"ea_chainmail");
 	public static final Item EA_CHAINMAIL_HELMET = new EAArmorItem(CH_MATERIAL, EquipmentSlotType.HEAD, new Properties().group(ItemGroup.COMBAT), "ea_chainmail_helmet");
 	public static final Item EA_CHAINMAIL_CHESTPLATE = new EAArmorItem(CH_MATERIAL, EquipmentSlotType.CHEST, new Properties().group(ItemGroup.COMBAT), "ea_chainmail_chestplate");
 	public static final Item EA_CHAINMAIL_LEGGINGS = new EAArmorItem(CH_MATERIAL, EquipmentSlotType.LEGS, new Properties().group(ItemGroup.COMBAT), "ea_chainmail_leggings");
@@ -35,9 +36,8 @@ public class ModItems
 	public static final Item EA_NETHERITE_CHESTPLATE = new EAArmorItem(NT_MATERIAL, EquipmentSlotType.CHEST, new Properties().group(ItemGroup.COMBAT).isImmuneToFire(), "ea_netherite_chestplate");
 	public static final Item EA_NETHERITE_LEGGINGS = new EAArmorItem(NT_MATERIAL, EquipmentSlotType.LEGS, new Properties().group(ItemGroup.COMBAT).isImmuneToFire(), "ea_netherite_leggings");
 	public static final Item EA_NETHERITE_BOOTS = new EAArmorItem(NT_MATERIAL, EquipmentSlotType.FEET, new Properties().group(ItemGroup.COMBAT).isImmuneToFire(), "ea_netherite_boots");
-    public static final Item EA_NETHERITE_SWORD = new EASwordItem(ItemTier.NETHERITE, 9, -4.0F, (new Item.Properties()).group(ItemGroup.COMBAT).isImmuneToFire(),"ea_netherite_sword");
+    public static final Item EA_NETHERITE_SWORD = new EASwordItem(ItemTier.NETHERITE, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT).isImmuneToFire(),"ea_netherite_sword");
 
-	
 	private static final IArmorMaterial IR_MATERIAL = new EAArmorMaterial(ArmorMaterial.IRON,"ea_iron");
 	public static final Item EA_IRON_HELMET = new EAArmorItem(IR_MATERIAL, EquipmentSlotType.HEAD, new Properties().group(ItemGroup.COMBAT), "ea_iron_helmet");
 	public static final Item EA_IRON_CHESTPLATE = new EAArmorItem(IR_MATERIAL, EquipmentSlotType.CHEST, new Properties().group(ItemGroup.COMBAT), "ea_iron_chestplate");
@@ -45,10 +45,8 @@ public class ModItems
 	public static final Item EA_IRON_BOOTS = new EAArmorItem(IR_MATERIAL, EquipmentSlotType.FEET, new Properties().group(ItemGroup.COMBAT), "ea_iron_boots");
     public static final Item EA_IRON_SWORD = new EASwordItem(ItemTier.IRON, 3, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT),"ea_iron_sword");
 
-	
 	public static void register(IForgeRegistry<Item> forgeRegistry)
 	{
-
 		forgeRegistry.registerAll(EA_HELMET, EA_CHESTPLATE, EA_LEGGINGS, EA_BOOTS, EA_DIAMOND_SWORD,
 				EA_IRON_HELMET,EA_IRON_CHESTPLATE,EA_IRON_LEGGINGS, EA_IRON_BOOTS, EA_IRON_SWORD,
 				EA_NETHERITE_HELMET,EA_NETHERITE_CHESTPLATE,EA_NETHERITE_LEGGINGS, EA_NETHERITE_BOOTS, EA_NETHERITE_SWORD,
